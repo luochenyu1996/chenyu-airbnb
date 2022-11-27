e729aa38993661a4525c7859b6b860da8edd9147
chenyu <luochneyu1996@qq.com>
2022-11-27 17:50:39 +0800

    HeaderCenter pannel码编写

--- src/components/app-header/c-cpns/header-right/index.js
+++ src/components/app-header/c-cpns/header-right/index.js
@@ -1,4 +1,4 @@
-import React, {memo} from "react";
+import React, {memo, useEffect, useState} from "react";
 import {HeaderRightWrapper} from "components/app-header/c-cpns/header-right/style";
 import Icon_global from "@/assets/svg/icon_global";
 import Icon_Avatar from "@/assets/svg/icon_ avatar";
@@ -8,6 +8,39 @@ import IconMenu from "@/assets/svg/icon_menu";
 
 
 const HeaderRight = memo(() => {
+
+    const [showPanel, setShowPanel] = useState(false)
+
+    /*副作用代码*/
+
+
+     useEffect(()=>{
+          function windowHandleClick(){
+              setShowPanel(false)
+          }
+
+         // 添加监听  todo 需要了解  参数 true 的作用，和事件冒泡相关
+         window.addEventListener("click",windowHandleClick,true)
+
+         //取消肩监听
+         return()=>{
+              window.removeEventListener("click",windowHandleClick,true)
+         }
+     },[])
+
+
+
+
+    /*事件处理函数*/
+
+     function profileClickHandle(){
+        setShowPanel(true)
+     }
+
+    // 监听窗口被点击
+
+
+
     return (
         <HeaderRightWrapper>
             <div className='btns'>
@@ -19,9 +52,26 @@ const HeaderRight = memo(() => {
 
             </div>
 
-            <div className="profile">
+            <div className="profile" onClick={profileClickHandle}>
                 <IconMenu/>
                 <IconAvatar/>
+                {
+                    showPanel && (
+                        <div className='panel'>
+                            <div className='top'>
+                                <div className='item'>注册</div>
+                                <div className='item'>登录</div>
+                            </div>
+                            <div className='bottom'>
+                                <div className='item'>出租房源</div>
+                                <div className='item'>开展体验</div>
+                                <div className='item'>帮助</div>
+                            </div>
+                        </div>
+                    )
+                }
+
+
             </div>
 
         </HeaderRightWrapper>
--- src/components/app-header/c-cpns/header-right/style.js
+++ src/components/app-header/c-cpns/header-right/style.js
@@ -1,8 +1,6 @@
 import styled from "styled-components";
 
 
-
-
 export const HeaderRightWrapper = styled.div`
   //  向右边靠的布局
   flex: 1;
@@ -33,6 +31,7 @@ export const HeaderRightWrapper = styled.div`
   }
 
   .profile {
+    position: relative;
     display: flex;
     justify-content: space-evenly;
     align-items: center;
@@ -40,13 +39,42 @@ export const HeaderRightWrapper = styled.div`
     height: 42px;
     margin-right: 24px;
     box-sizing: border-box;
-    border: 1px solid #ccc; 
+    border: 1px solid #ccc;
     border-radius: 25px;
     background-color: #fff;
     color: ${props => props.theme.text.primaryColor};
     cursor: pointer;
     //头像阴影和动画
-    ${props=> props.theme.mixin.boxShadow};
+    ${props => props.theme.mixin.boxShadow};
+  }
+
+  .panel {
+    position: absolute;
+    right: 0;
+    top: 60px;
+    width: 240px;
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 0 6px rgba(0, 0, 0, .18);
+    color: #666;
+
+    .top, .bottom {
+      padding: 8px 0;
+      .item {
+        height: 40px;
+        line-height: 40px;
+        padding: 0 16px;
+        &:hover{
+          background-color: #f5f5f5;
+        }
+      }
+
+    }
+    .top{
+        border-bottom: 1px solid  #ddd;
+    }
+
+
   }
 
 `
