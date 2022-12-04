38518aa89d5e6ba5c4ec0e4c6179a56da8ee9fc0
chenyu <luochneyu1996@qq.com>
2022-12-04 21:51:05 +0800

    封装home-section-v3组件

---
+++ src/views/home/c-cpns/home-section-v3/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+export const  HomeSectionV3Wrapper=styled.div`
+  .room-list {
+    margin: 0 -8px;
+    /* display: flex;
+    flex-wrap: wrap; */
+  }
+
+`
