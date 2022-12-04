46a2255fb5cf40e56e7a5a0926776197f3ca7a4f
chenyu <luochneyu1996@qq.com>
2022-12-04 22:31:26 +0800

    修改yearning-item组件

--- src/components/yearning-item/index.jsx
+++ src/components/yearning-item/index.jsx
@@ -1,6 +1,25 @@
-import React ,{memo} from "react";
+import React, {memo} from "react";
+import {YearningWrapper} from "components/yearning-item/styled";
 
-const  YearningForItem=memo((props)=>{
 
+const YearningItem = memo((props) => {
+    const {itemData} = props
 
+    return (
+        <YearningWrapper>
+            <div className='inner'>
+                <div className='item-info'>
+                    <img className='cover' src={itemData.picture_url} alt=""/>
+                    <div className='bg-cover'></div>
+                    <div className='info'>
+                        <div className='city'>{itemData.city}</div>
+                        <div className='price'>均价 {itemData.price}</div>
+                    </div>
+                </div>
+            </div>
+        </YearningWrapper>
+
+    )
 })
+
+export default YearningItem
