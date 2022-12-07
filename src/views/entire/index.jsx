fd3cbee74a4daa3582002be113fc7b1d519d7794
chenyu <luochneyu1996@qq.com>
2022-12-08 01:29:24 +0800

    封装EntireFilter组件

--- src/views/entire/index.jsx
+++ src/views/entire/index.jsx
@@ -1,10 +1,13 @@
 import React,{memo} from "react";
 import {EntireWrapper} from "@/views/entire/style";
+import {EntireFilterWrapper} from "@/views/entire/c-cpns/entire-filter/style";
+import EntireFilter from "@/views/entire/c-cpns/entire-filter";
 
 
 const  Entire =memo(()=>{
     return(
          <EntireWrapper>
+             <EntireFilter/>
              <div  className='filter'>  </div>
              <div  className='rooms'>  </div>
              <div  className='pagination'>  </div>
