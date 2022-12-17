73a7b8e9db2921a16be0a4e6b3463deef10ed5bb
chenyu <luochneyu1996@qq.com>
2022-12-17 17:16:47 +0800

    调整箭头样式

--- src/components/room-item/styled.js
+++ src/components/room-item/styled.js
@@ -29,7 +29,43 @@ export const RoomItemWrapper = styled.div`
       top: 0;
       width: 100%;
       height: 100%;
-      object-fit:  cover;
+      object-fit: cover;
+    }
+  }
+
+  .slider {
+    position: relative;
+    cursor: pointer;
+
+    &:hover {
+      .control {
+        display: flex;
+      }
+    }
+
+    .control {
+      position: absolute;
+      z-index: 1;
+      right: 0;
+      left: 0;
+      top: 0;
+      bottom: 0;
+      display: none;
+      justify-content: space-between;
+      color: #ffd;
+
+      .btn {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 83px;
+        height: 100%;
+        background: linear-gradient(to left, transparent 0%, rgba(0, 0, 0, 0.25) 100%);
+
+        &.right {
+          background: linear-gradient(to right, transparent 0%, rgba(0, 0, 0, 0.25) 100%);
+        }
+      }
     }
   }
 
@@ -37,14 +73,14 @@ export const RoomItemWrapper = styled.div`
     margin: 10px 0 5px;
     font-size: 12px;
     font-weight: 700;
-     color: ${props => props.verifyColor};
+    color: ${props => props.verifyColor};
   }
 
-  .name  {
+  .name {
     font-size: 16px;
     font-weight: 700;
     overflow: hidden;
-    text-overflow: ellipsis;  //内容溢出样式
+    text-overflow: ellipsis; //内容溢出样式
     display: -webkit-box;
     -webkit-line-clamp: 2;
     -webkit-box-orient: vertical;
@@ -54,7 +90,7 @@ export const RoomItemWrapper = styled.div`
     margin: 8px 0;
   }
 
-  .button { 
+  .button {
     display: flex;
     align-items: center;
     font-size: 12px;
@@ -64,16 +100,17 @@ export const RoomItemWrapper = styled.div`
     .count {
       margin: 0 2px 0 4px;
     }
+
     // 这个属性根据 MUI 的源码得出
     .MuiRating-icon {
-      
+
       font-size: 12px;
       margin-right: -2px;
       color: #00848A;
     }
   }
-  
-  
+
+
 
 
 `
