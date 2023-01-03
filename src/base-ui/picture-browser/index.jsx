19b5c0c4904c3ecb3090fa432eb7d20e73af6352
chenyu <luochneyu1996@qq.com>
2023-01-03 22:07:11 +0800

    优化底部图片浏览器

--- src/base-ui/picture-browser/index.jsx
+++ src/base-ui/picture-browser/index.jsx
@@ -1,11 +1,21 @@
-import React, {memo, useEffect} from "react";
+import React, {memo, useEffect, useState} from "react";
 import {PictureBrowserWrapper} from "@/base-ui/picture-browser/styled";
 import PropTypes from "prop-types";
+import {CSSTransition, SwitchTransition} from "react-transition-group";
+
 import IconClose from "@/assets/svg/icon-close";
+import IconArrowLeft from "@/assets/svg/icon-arrow-left";
+import IconArrowRight from "@/assets/svg/icon-arrow-right";
+import IconTriangleDown from "@/assets/svg/icon-triangle-down";
+import Indicator from "@/base-ui/indicator";
+import classNames from "classnames";
 
 const PictureBrowser = memo((props) => {
 
-    const {pictureUrls,closeClick}=props
+    const {pictureUrls, closeClick} = props
+
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const  [showList, setShowList]=useState(true)
 
     // 当图片浏览器展示时候，窗口的滚动功能应该消失
     useEffect(() => {
@@ -19,23 +29,95 @@ const PictureBrowser = memo((props) => {
 
 
     // 关闭事件逻辑
-    function closeBtnClickHandle(){
-        if(closeClick) closeClick()
+    function closeBtnClickHandle() {
+        if (closeClick) closeClick()
+    }
+
+    function controlClickHandle(isNext = true) {
+        let newIndex = isNext ? currentIndex + 1 : currentIndex - 1
+        if (newIndex < 0) newIndex = pictureUrls?.length - 1
+        if (newIndex > pictureUrls?.length - 1) newIndex = 0
+        setCurrentIndex(newIndex)
+    }
+
+    function toggleShowListHandle(){
+        setShowList(false);
     }
 
 
     return (
 
         <PictureBrowserWrapper>
-            <div  className='top'>
+            <div className='top'>
                 <div className='close-btn' onClick={closeBtnClickHandle}>
                     <IconClose/>
                 </div>
             </div>
 
-            <div className='list'></div>
-            <div className='indicator'></div>
+            <div className='slider'>
+                <div className='control'>
+                    <div className='btn left' onClick={e => controlClickHandle(false)}>
+                        <IconArrowLeft width={77} height={77}/>
+                    </div>
+                    <div className='btn right' onClick={e => controlClickHandle(true)}>
+                        <IconArrowRight width={77} height={77}/>
+                    </div>
+                </div>
+                <div className='picture'>
+                    <SwitchTransition mode='in-out'>
+                        <CSSTransition
+                            classNames='pic'
+                            timeout={200}
+                            key={pictureUrls[currentIndex]}>
+                            <img src={pictureUrls?.[currentIndex]} alt=""/>
+                        </CSSTransition>
+                    </SwitchTransition>
 
+
+                </div>
+
+            </div>
+
+
+            <div className='preview'>
+                <div className='info'>
+                    <div className='desc'>
+                        <div className='count'>
+                            <span>{currentIndex+1}/{pictureUrls?.length}:</span>
+                            <span>room Apartment 图片{currentIndex+1}</span>
+                        </div>
+                        <div className='toggle' onClick={toggleShowListHandle}>
+                            隐藏照片列表
+                            {
+                                showList&&(
+                                    <IconTriangleDown/>
+                                )
+                            }
+
+
+                            <IconTriangleDown/>
+                        </div>
+
+                    </div>
+                    <div className='list'>
+                        <Indicator selectIndex={currentIndex}>
+                            {
+                                pictureUrls.map((item,index) => {
+                                    return (
+                                        <div className={classNames("item",{active:currentIndex===index})}
+                                             key={item}>
+                                            <img src={item} alt=""/>
+                                        </div>
+                                    )
+                                })
+                            }
+
+                        </Indicator>
+                    </div>
+
+
+                </div>
+            </div>
 
         </PictureBrowserWrapper>
 
@@ -44,7 +126,7 @@ const PictureBrowser = memo((props) => {
 
 })
 
-PictureBrowser.propTypes={
-    pictureUrls:PropTypes.array
+PictureBrowser.propTypes = {
+    pictureUrls: PropTypes.array
 }
 export default PictureBrowser
--- src/base-ui/picture-browser/styled.js
+++ src/base-ui/picture-browser/styled.js
@@ -7,26 +7,120 @@ export const PictureBrowserWrapper = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
-  background-color:#333;
-  
+  background-color: #333;
+
   display: flex;
   flex-direction: column;
-  
-  .top{
+
+  .top {
     position: relative;
     height: 86px;
-    
-    .close-btn{
+
+    .close-btn {
       position: absolute;
-      top:15px;
+      top: 15px;
       right: 25px;
-      
     }
   }
-  
-  .slider{
+
+  .slider {
     position: relative;
     display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    overflow: hidden;
+
+    .control {
+      position: absolute;
+      z-index: 1;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      display: flex;
+      justify-content: space-between;
+      color: #fff;
+
+      .btn {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 83px;
+        height: 100%;
+        cursor: pointer;
+      }
+    }
+
+    .picture {
+      position: relative;
+      height: 100%;
+      overflow: hidden;
+      width: 100% !important;
+      max-width: 105vh !important;
+
+      img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        margin: 0 auto;
+        height: 100%;
+        user-select: none;
+      }
+
+      //图片切换动画
+      .pic-enter {
+        transform: translateX(100%);
+        opacity: 0;
+      }
+
+      .pic-enter-active {
+        transform: translate(0);
+        opacity: 1;
+        transition: all 200ms ease;
+      }
+
+      .pic-exit {
+        opacity: 1;
+      }
+
+      .pic-exit-active {
+        opacity: 0;
+        transition: all 200ms ease;
+      }
+
+    }
+
+
   }
-  
+
+  .preview {
+    display: flex;
+    justify-content: center;
+    height: 100px;
+    margin-top: 10px;
+
+    .info {
+      position: absolute;
+      bottom: 10px;
+      max-width: 105vh;
+      
+      .desc{
+        display: flex;
+        justify-content: space-between;
+      }
+      img{
+        
+      }
+      
+    }
+
+  }
+
+
+
+
+
+
 `
